f8e8f2e9f00fc0aed3342dfe7010019896e36160
Caio Mello <caio-mello94@hotmail.com>
2024-06-12 16:03:26 -0300

    Delete Desafios/AulasRuby directory

--- Desafios/AulasRuby/mercadinho/app.rb
+++
@@ -1,9 +0,0 @@
-
-require_relative 'supermarket'
-require_relative 'product'
-
-product = Product.new( name: 'PEPSI TWIST 2LT', price: 8.00 )
-Supermarket.new(product.name, product.price).purchase
-
-product = Product.new(name: 'PRINGLES', price: 13.00)
-Supermarket.new(product.name, product.price).purchase
--- Desafios/AulasRuby/mercadinho/product.rb
+++
@@ -1,9 +0,0 @@
-
-class Product 
-    attr_accessor :name, :price
-
-    def initialize ( name:, price: )
-        @name = name
-        @price = price
-    end
-end
